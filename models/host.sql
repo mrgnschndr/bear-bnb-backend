44af2aee25e2bd704ec12fbff23b7b9c25cd3788
krissyjames <kristina.ashley.cook@gmail.com>
2024-12-31 23:04:35 -0500

    host model

--- models/host.sql
+++ models/host.sql
@@ -1,3 +1,10 @@
 CREATE TABLE IF NOT EXISTS host (
-    id SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
+    host_id SERIAL
+    is_superhost BOOLEAN
+    number_reviews INTEGER
+    host_rating SMALLINT
+    date_hosted DATE
+    listing_id SERIAL
+    host_bio TEXT
 );
