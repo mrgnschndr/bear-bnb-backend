e805b1029de750b2801fd6167c82605f54c91ad3
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2025-01-02 22:54:09 -0500

    faker data updated

--- seeding/userSeeding.js
+++ seeding/userSeeding.js
@@ -7,20 +7,22 @@ const seedUsers = async () => {
   // TODO: Create 100 Users 10 have to be host
   for (let i = 0; i < 100; i++) {
     
-    const user_first_name = faker.name.findName();
-    const user_last_name = faker.internet.email();
-    const user_city = faker.location.city();
-    const user_state = faker.location.;
-    const user_country = ;
-    const is_logged_in = ;
-    const is_host = ;
-    const user_email = ;
-    const user_birth_month = ;
-    const user_birth_day = ;
-    const user_birth_year = ;
-    const user_phone =;
-    const user_image_url = ;
-    users.push({ name, email, createdAt });
+    const user_first_name = faker.name.firstName();
+    const user_last_name = faker.name.lastName();
+    const user_city = faker.address.city();
+    const user_state = faker.address.state();
+    const user_country = faker.address.country();
+    const is_logged_in = false;
+    
+    // Randomly decide if this user will be a host (10 out of 100)
+    const is_host = i < 10; // First 10 users are hosts
+
+    const user_email = faker.internet.email(user_first_name, user_last_name);
+    const user_birth_month = faker.date.past().getMonth() + 1; // 1-12
+    const user_birth_day = faker.date.past().getDate(); // 1-31
+    const user_birth_year = faker.date.past().getFullYear(); // Get a past year (for the birth year)
+    const user_phone = faker.phone.phoneNumber();
+    const user_image_url = faker.image.avatar(); // Random avatar image URL
   }
 
   try {
