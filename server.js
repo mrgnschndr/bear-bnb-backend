4f1320040d75d594e64d2ba64236d099a8be6a9b
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2025-01-22 22:58:46 -0500

    Auth (#43)
    * auth testing
    
    * User profile middleware added
    
    * updated server file base url

--- server.js
+++ server.js
@@ -1,30 +1,53 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config(); 
+require('dotenv').config();
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
 // Import routes
 const usersRouter = require('./routes/userRoutes');
-const listingsRouter = require('./routes/listingRoutes')
-const reviewsRouter = require('./routes/reviewsRoute')
-
-const hostRouter = require('./routes/hostRoutes')
+const listingsRouter = require('./routes/listingRoutes');
+const reviewsRouter = require('./routes/reviewsRoute');
+const hostRouter = require('./routes/hostRoutes');
 
 const app = express();
-const port = process.env.PORT || 5004; 
+const port = process.env.PORT || 5004;
+
+// Configuration for Auth0
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: 'http://localhost:5004',
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+};
 
 // Middleware
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
+
+// Route to get user profile
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+// Auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// Route to check authentication status
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 // Routes
 app.use(usersRouter);
 app.use(listingsRouter);
 app.use(reviewsRouter);
-
 app.use(hostRouter);
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`); 
-  console.log('Airbnb Clone Backend is live!'); 
+  console.log(`Server listening on port ${port}`);
+  console.log('Airbnb Clone Backend is live!');
 });
