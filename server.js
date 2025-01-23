11f4cda7aa1c76d070d81f27f9ce1ce3c89e5554
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2025-01-22 22:14:14 -0500

    updated server file base url

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const config = {
   secret: process.env.AUTH0_SECRET,
   baseURL: 'http://localhost:5004',
   clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: 'https://dev-4shamkfdqnw71u10.us.auth0.com'
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
 };
 
 // Middleware
