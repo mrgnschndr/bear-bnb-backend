dafde3310137ebc8768ac91e9ed42a57b986a386
mrgnschndr <mrgnschndr@gmail.com>
2025-02-02 08:59:12 -0600

    Initial commit from fork - removed auth

--- server.js
+++ server.js
@@ -1,49 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-const { auth } = require('express-openid-connect');
-const { requiresAuth } = require('express-openid-connect');
+require('dotenv').config(); 
 
 // Import routes
 const usersRouter = require('./routes/userRoutes');
-const listingsRouter = require('./routes/listingRoutes');
-const reviewsRouter = require('./routes/reviewsRoute');
-const hostRouter = require('./routes/hostRoutes');
+const listingsRouter = require('./routes/listingRoutes')
+const reviewsRouter = require('./routes/reviewsRoute')
+
+const hostRouter = require('./routes/hostRoutes')
 
 const app = express();
-const port = process.env.PORT || 5004;
-
-// Configuration for Auth0
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.AUTH0_SECRET,
-  baseURL: 'http://localhost:5004',
-  clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
-};
+const port = process.env.PORT || 5004; 
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-
-// Route to get user profile
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
-
-// Auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// Route to check authentication status
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+app.use(cors()); 
+app.use(express.json()); 
 
 // Routes
 app.use(usersRouter);
 app.use(listingsRouter);
 app.use(reviewsRouter);
+
 app.use(hostRouter);
 
 // Start the server
@@ -51,5 +28,3 @@ app.listen(port, () => {
   console.log(`Server listening on port ${port}`); 
   console.log('Airbnb Clone Backend is live!'); 
 });
-
-module.exports = app;
